915dd7cdd81e7f6ae495b3e96e9e6bee73ed859e
KrystianJM <krystianjm@gmail.com>
2019-01-04 20:55:14 +0000

    add title_helper and make cleaner gemfile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+#Title for pages that by accident may not have one
+  def full_title(page_title = '')
+    base_title = "Fabulari"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
